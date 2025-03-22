b0abd0a4ae6f3b31e77bada2eaa49f5df63c90d2
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-03-22 14:24:33 -0300

    Update api.js

--- src/server/api.js
+++ src/server/api.js
@@ -6,7 +6,9 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors({
-  origin: 'https://estoque-golas-f8742hhsx-cleilsons-projects.vercel.app'
+  origin: 'https://estoque-golas-cbxr311zu-cleilsons-projects.vercel.app', // Domínio do frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
 }));
 app.use(express.json());
 
