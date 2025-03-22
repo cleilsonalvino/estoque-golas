36d7b704ea8bb03baafc8019fcc9c3cc8c6b56a9
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-03-22 15:02:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['estoque-golas.onrender.com'], // Permite o host especificado
-  },
-  base: '/', // Adicione essa linha para garantir que os recursos sejam encontrados corretamente
+  
 })
