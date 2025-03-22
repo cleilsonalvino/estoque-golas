786b4d15263f5f73b45ceea31ebff1c5c56b3054
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-03-22 16:01:49 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  root: './' // Isso faz o Vite procurar o arquivo index.html na raiz do projeto
 })
