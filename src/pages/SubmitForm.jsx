d89d6f79d1afcaa74bc66dcf07c3362bd7f4a67d
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-03-31 23:38:19 -0300

    Update SubmitForm.jsx

--- src/pages/SubmitForm.jsx
+++ src/pages/SubmitForm.jsx
@@ -17,7 +17,7 @@ function SubmitForm() {
 
   // Carregar os polos disponíveis
   useEffect(() => {
-    fetch("https://3.17.153.198:4000/trazer-dados") // Chame sua API para obter os polos
+    fetch("http://3.17.153.198:4000/trazer-dados") // Chame sua API para obter os polos
       .then((response) => response.json())
       .then((data) => {
         setPolos(data);
@@ -51,7 +51,7 @@ function SubmitForm() {
 
     try {
       const response = await fetch(
-        "https://3.17.153.198:4000/estoque",
+        "http://3.17.153.198:4000/estoque",
         {
           method: "POST",
           headers: {
